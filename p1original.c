bce131999091cd45d99fbde861a6300b36b4a0d5
Shalyn Maria <95674587+Shal67@users.noreply.github.com>
2022-03-28 09:19:11 +0000

    xyz1

--- p1original.c
+++ p1original.c
@@ -1,35 +1,31 @@
-#include<stdio.h>
-void input(int *num1,int *den1,int *num2,int *den2)
-{
-  printf("Enter the numerator: \n");
-  scanf("%d",num1);
-  printf("Enter the denominator: \n");
-  scanf("%d",den1);
-  printf("Enter the numerator: \n");
-  scanf("%d",num2);
-  printf("Enter the denominator: \n");
-  scanf("%d",den2);
+#include <stdio.h>
+void input(int *num1, int *den1, int *num2, int *den2) {
+	printf("Enter the numerator: \n");
+	scanf("%d", num1);
+	printf("Enter the denominator: \n");
+	scanf("%d", den1);
+	printf("Enter the numerator: \n");
+	scanf("%d", num2);
+	printf("Enter the denominator: \n");
+	scanf("%d", den2);
 }
-void add(int num1,int den1,int num2,int den2,int *num3,int *den3)
-{
-  int i,gcd;
-  *num3=(num1*den2)+(num2*den1);
-  *den3=(den1*den2);
-  for(i=1;i<=100;i++)
-    if(*num3%i==0 && *den3%i==0)
-      gcd=i;
-  *num3=*num3/gcd;
-  *den3=*den3/gcd;
+void add(int num1, int den1, int num2, int den2, int *num3, int *den3) {
+	int i, gcd;
+	*num3 = (num1 * den2) + (num2 * den1);
+	*den3 = (den1 * den2);
+	for (i = 1; i <= 100; i++)
+		if (*num3 % i == 0 && *den3 % i == 0)
+			gcd = i;
+	*num3 = *num3 / gcd;
+	*den3 = *den3 / gcd;
 }
-void output(int num1,int den1,int num2,int den2,int num3,int den3)
-{
-  printf("%d/%d + %d/%d = %d/%d",num1,den1,num2,den2,num3,den3);
+void output(int num1, int den1, int num2, int den2, int num3, int den3) {
+	printf("%d/%d + %d/%d = %d/%d", num1, den1, num2, den2, num3, den3);
 }
-int main()
-{
-  int num1,den1,num2,den2,num3,den3;
-  input(&num1,&den1,&num2,&den2);
-  add(num1,den1,num2,den2,&num3,&den3);
-  output(num1,den1,num2,den2,num3,den3);
-  return 0;
-} 
+int main() {
+	int num1, den1, num2, den2, num3, den3;
+	input(&num1, &den1, &num2, &den2);
+	add(num1, den1, num2, den2, &num3, &den3);
+	output(num1, den1, num2, den2, num3, den3);
+	return 0;
+}
